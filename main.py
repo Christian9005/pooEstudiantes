9c81c758b3a494ff2dcf241a6e503a8c0f01b51b
Christian Albarracin <christianalex-071@hotmail.com>
2023-03-13 21:54:56 -0500

    third commit

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ class Estudiante:
         conectar = conectar_db()
         base_datos = conectar.cursor()
         estudiante_valido = validar_estudiante(self.cedula)
-        if estado_valido:
+        if estudiante_valido:
             sql = "INSERT INTO estudiantes (cedula, nombre, apellido, correo, celular) VALUES (%s, %s, %s, %s, %s)"
             val = (self.cedula, self.nombre, self.apellido, self.correo, self.celular)
             base_datos.execute(sql, val)
@@ -145,9 +145,9 @@ class Estudiante:
 
     def buscar_estudiante(self):
         estudiante_valido = validar_estudiante(self.cedula)
-        if not estado_valido:
+        if not estudiante_valido:
             print("Estudiante Encontrado")
-            conectar = Connection_DB()
+            conectar = conectar_db()
             base_datos = conectar.cursor()
             cedula_estudiante = (f"{self.cedula}",)
             sql = "SELECT * FROM estudiantes WHERE cedula= %s"
@@ -161,7 +161,7 @@ class Estudiante:
 
     def modificar_estudiante(self):
         estudiante_valido = validar_estudiante(self.cedula)
-        if not estado_valido:
+        if not estudiante_valido:
             print("Estudiante Encontrado")
             print("Ingrese las credenciales del estudiante a modificar")
             cedula = input("Cedula: ")
@@ -171,7 +171,7 @@ class Estudiante:
             celular = input("Celular: ")
             confirmacion = input("Confirme la actualizacion del estudiante (S/N): ")
             if confirmacion.upper() == "S":
-                conector = conectar_db()
+                conectar = conectar_db()
                 base_datos = conectar.cursor()
                 estudiante_modificado = (f"{cedula}", f"{nombre}", f"{apellido}", f"{correo}", f"{celular}", f"{self.cedula}")
                 sql = "UPDATE estudiantes set cedula= %s, nombre= %s, apellido= %s, correo= %s, celular= %s WHERE cedula= %s"
@@ -187,7 +187,7 @@ class Estudiante:
 
     def eliminar_estudiante(self):
         estudiante_valido = validar_estudiante(self.cedula)
-        if not estado_valido:
+        if not estudiante_valido:
             print("Estudiante Encontrado")
             confirmacion = input("Confirme la eliminacion del estudiante (S/N): ")
             if confirmacion.upper() == "S":
@@ -198,7 +198,7 @@ class Estudiante:
                 base_datos.execute(sql, cedula)
                 conectar.commit()
                 print("Usuario eliminado exitosamente")
-            elif confirmation.upper() == "N":
+            elif confirmacion.upper() == "N":
                 print("Estudiante no eliminado")
             else:
                 print("Opcion no valida")
@@ -219,7 +219,92 @@ class Calificaciones(Estudiante):
 
         self.promedio = (nota1 + nota2 + nota3 + nota4) / 4
 
+    def registrar_notas(self):
+        conectar = conectar_db()
+        base_datos = conectar.cursor()
+        estudiante_valido = validar_estudiante(self.cedula)
+        if estudiante_valido:
+            sql = "INSERT INTO calificaciones (cedula, nombre, apellido, correo, celular, promedio) VALUES (%s, %s, %s, %s, %s, %s)"
+            val = (self.cedula, self.nombre, self.apellido, self.correo, self.celular, self.promedio)
+            base_datos.execute(sql, val)
+            conectar.commit()
+            print(f"Notas del Estudiante con CI: {self.cedula} creadas exitosamente")
+        else:
+            print("Error Estudiante duplicado")
 
+    def listar_notas(self):
+        print("Notas de los Estudiantes")
+        conectar = conectar_db()
+        base_datos = conectar.cursor()
+        sql = "SELECT * FROM calificaciones"
+        base_datos.execute(sql)
+        notas_estudiantes = base_datos.fetchall()
+        for nota in notas_estudiantes:
+            print("Id     Cedula     Promedio")
+            print(f"{nota[0]}      {nota[1]}       {nota[6]}")
+
+    def buscar_notas_estudiante(self):
+        estudiante_valido = validar_estudiante(self.cedula)
+        if not estudiante_valido:
+            print("Estudiante Encontrado")
+            conectar = conectar_db()
+            base_datos = conectar.cursor()
+            cedula_estudiante = (f"{self.cedula}",)
+            sql = "SELECT * FROM calificaciones WHERE cedula= %s"
+            base_datos.execute(sql, cedula_estudiante)
+            nota_estudiante = base_datos.fetchall()
+            for nota in nota_estudiante:
+                print("Id     Cedula     Promedio")
+                print(f"{nota[0]}      {nota[1]}       {nota[6]}")
+        else:
+            print("Estudiante no registrado!!!")
+
+    def modificar_notas(self):
+        estudiante_valido = validar_estudiante(self.cedula)
+        if not estudiante_valido:
+            print("Estudiante Encontrado")
+            print("Ingrese las notas del estudiante a modificar")
+            nota1 = Validacion_Nota(1)
+            nota2 = Validacion_Nota(2)
+            nota3 = Validacion_Nota(3)
+            nota4 = Validacion_Nota(4)
+            print(f"Notas {nota1}, {nota2}, {nota3}, {nota4}")
+            confirmation = input(f"Confirme las notas ingresado. (S/N): ")
+            if confirmation.upper() == "S":
+                self.promedio = (nota1 + nota2 + nota3 + nota4) // 4
+                conectar = conectar_db()
+                base_datos = conectar.cursor()
+                estudiante_modificado = (f"{self.cedula}", f"{self.promedio}", f"{self.cedula}")
+                sql = "UPDATE calificaciones set cedula= %s, promedio= %s WHERE cedula= %s"
+                base_datos.execute(sql, estudiante_modificado)
+                conectar.commit()
+                print("Notas del estudiante modificadas exitosamente")
+            elif confirmation.upper() == "N":
+                print("Notas del estudiante no modificadas")
+            else:
+                print("Opcion no valida")
+        else:
+            print("Estudiante no registrado!!!")
+
+    def Delete_Student_Scores(self):
+        estudiante_valido = validar_estudiante(self.cedula)
+        if not estudiante_valido:
+            print("Estudiante Encontrado")
+            confirmacion = input("Confirme la eliminacion de las notas del estudiante (S/N): ")
+            if confirmacion.upper() == "S":
+                conectar = conectar_db()
+                base_datos = conectar.cursor()
+                cedula = (f"{self.cedula}",)
+                sql = "DELETE FROM calificaciones WHERE cedula= %s"
+                base_datos.execute(sql, cedula)
+                conectar.commit()
+                print("Notas eliminadas exitosamente")
+            elif confirmacion.upper() == "N":
+                print("Notas no eliminadas")
+            else:
+                print("Opcion no valida")
+        else:
+            print("Estudiante no registrado!!!")
 
 
 
